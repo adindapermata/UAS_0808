f9637f2cb956d1731b42cb85f7efeb8abece501a
adindapermata <adinda.a@students.amikom.ac.id>
2021-01-18 01:14:14 +0700

    UAS PCS

--- app/src/main/java/com/adindapermata/github0808/api/Api.kt
+++ app/src/main/java/com/adindapermata/github0808/api/Api.kt
@@ -8,25 +8,25 @@ import retrofit2.http.*
 
 interface Api {
     @GET("search/users")
-    @Headers("Authorization: token daa1fb3f4a920d5e09b86ee45f008612eba8b3d1")
+    @Headers("Authorization: token 274ba64606c9a1ce39dc8dc56b78b1128e21edfb")
     fun getSearchUsers(
         @Query("q") query: String
     ): Call<UserResponse>
 
     @GET("users/{username}")
-    @Headers("Authorization: token daa1fb3f4a920d5e09b86ee45f008612eba8b3d1")
+    @Headers("Authorization: token 274ba64606c9a1ce39dc8dc56b78b1128e21edfb")
     fun getUserDetail(
         @Path("username") username: String
     ): Call<DetailUserResponse>
 
     @GET("users/{username}/followers")
-    @Headers("Authorization: token daa1fb3f4a920d5e09b86ee45f008612eba8b3d1")
+    @Headers("Authorization: token 274ba64606c9a1ce39dc8dc56b78b1128e21edfb")
     fun getFollowers(
         @Path("username") username: String
     ): Call<ArrayList<User>>
 
     @GET("users/{username}/following")
-    @Headers("Authorization: token daa1fb3f4a920d5e09b86ee45f008612eba8b3d1")
+    @Headers("Authorization: token 274ba64606c9a1ce39dc8dc56b78b1128e21edfb")
     fun getFollowing(
         @Path("username") username: String
     ): Call<ArrayList<User>>
